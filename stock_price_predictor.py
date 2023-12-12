dc0bacaebc4b020cb9ee16d126ccc8f8229a72ea
Vivek Gogi <vivekgogi123vg@gmail.com>
2023-12-12 08:41:32 +0530

    Imported Tenserflow Module

--- stock_price_predictor.py
+++ stock_price_predictor.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 from keras.layers import LSTM, Dense, Dropout
 from keras.models import Sequential
 from sklearn.preprocessing import MinMaxScaler
