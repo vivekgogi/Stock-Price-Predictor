3a357027ce20bb87b6307406f5e2a40671be2600
Vivek Gogi <vivekgogi123vg@gmail.com>
2023-12-12 09:10:00 +0530

    Modified stock_price_predictor.py

--- stock_price_predictor.py
+++ stock_price_predictor.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import tensorflow as tf
 from keras.layers import LSTM, Dense, Dropout
 from keras.models import Sequential
 from sklearn.preprocessing import MinMaxScaler
